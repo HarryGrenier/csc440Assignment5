df1762b1c969a35ace7a75a0c5c74a426e876d78
Harry Grenier <hgrenier@uri.edu>
2024-11-19 14:00:46 -0500

    naive implementation

--- resizeable_image.py
+++ resizeable_image.py
@@ -1,8 +1,30 @@
 import imagematrix
 
 class ResizeableImage(imagematrix.ImageMatrix):
-    def best_seam(self, dp=True):
-        raise NotImplemented
+    def best_seam(self, dp=False):
+        width, height = self.width, self.height
 
-    def remove_best_seam(self):
-        self.remove_seam(self.best_seam())
+        def compute_seam_energy(seam):
+            """Compute total energy for a given seam."""
+            return sum(self.energy(x, y) for x, y in seam)
+
+        def enumerate_seams(x, y):
+            """Recursively generate all seams starting from pixel (x, y)."""
+            if y == height - 1:
+                return [[(x, y)]]
+            
+            seams = []
+            for dx in [-1, 0, 1]:  # Explore (left-diagonal, vertical, right-diagonal)
+                nx = x + dx
+                if 0 <= nx < width:
+                    for seam in enumerate_seams(nx, y + 1):
+                        seams.append([(x, y)] + seam)
+            return seams
+        
+        all_seams = []
+        for x in range(width):
+            all_seams.extend(enumerate_seams(x, 0))
+
+        # Find the seam with the minimum energy
+        best_seam = min(all_seams, key=compute_seam_energy)
+        return best_seam
